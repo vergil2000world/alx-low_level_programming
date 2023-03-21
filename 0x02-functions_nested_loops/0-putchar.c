4da242bb329d455181cb749817a4a92735c1a845
vergil2000world <benmhamedchakib@gmail.com>
2023-03-21 06:14:34 -0700

    using putchat to prints

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
+#include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - program that prints _putchar.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: int
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	int n = 0;
+	char str_putchar[8] = "Holberton";
+
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
 }
